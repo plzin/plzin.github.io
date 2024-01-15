a1e3d3f829a95d2a2024b25ff84db29bd7eca06a
Justus Polzin <justuspolzin@gmail.com>
2024-01-15 10:30:28 +0100

    Fix indentation. Add social media links. Change link style. Fix linear MBA link at the end of MBA deobfuscation post.

--- components/layout.js
+++ components/layout.js
@@ -3,57 +3,76 @@ import styles from './layout.module.scss'
 import utilStyles from '../styles/utils.module.scss'
 import Link from 'next/link'
 import ThemeSwitcher from './theme-switcher'
+import { IoLogoTwitter, IoLogoGithub } from 'react-icons/io5'
 
 const name = 'Justus Polzin'
-export const siteTitle = 'Blog'
+export const siteTitle = 'Justus Polzin\'s Blog'
 
 export default function Layout({ children, home }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src="/images/profile.png"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <ThemeSwitcher />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <img
-                src="/images/profile.png"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt={name}
-              />
-            </Link>
-            <ThemeSwitcher />
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                {name}
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            ← Back to home
-          </Link>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <img
+                            src="/images/profile.png"
+                            className={utilStyles.borderCircle}
+                            height={144}
+                            width={144}
+                            alt={name}
+                        />
+                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                        <a href="https://twitter.com/justuspolzin" target="_blank">
+                            <div className={styles.socials}>
+                                <IoLogoTwitter size={30} />
+                                <div className={styles.socialHandle}>
+                                    @justuspolzin
+                                </div>
+                            </div>
+                        </a>
+                        <a href="https://github.com/plzin" target="_blank">
+                            <div className={styles.socials}>
+                                <IoLogoGithub size={30} />
+                                <div className={styles.socialHandle}>
+                                    @plzin
+                                </div>
+                            </div>
+                        </a>
+                        <ThemeSwitcher />
+                    </>
+                ) : (
+                    <>
+                        <div>
+                            <Link href="/">
+                                <img
+                                    src="/images/profile.png"
+                                    className={utilStyles.borderCircle}
+                                    height={108}
+                                    width={108}
+                                    alt={name}
+                                />
+                            </Link>
+                            <h2 className={utilStyles.headingLg}>
+                                <Link href="/" className={utilStyles.noDecoration}>
+                                    {name}
+                                </Link>
+                            </h2>
+                        </div>
+                        <ThemeSwitcher />
+                    </>
+                )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/" className={utilStyles.noDecoration}>
+                        ← Back to home
+                    </Link>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  )
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -12,15 +12,12 @@ export default function Home({ postsData, style }) {
                 <title>{siteTitle}</title>
                 <style dangerouslySetInnerHTML={{ __html: style }}></style>
             </Head>
-            <section className={utilStyles.headingMd}>
-                <p>Blog about computers, numbers and solarpunk.</p>
-            </section>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-                <h2 className={`${utilStyles.headingLg}`}>Blog</h2>
+                <h2 className={utilStyles.headingLg}>Posts</h2>
                 <ul className={utilStyles.list}>
                     {postsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
-                            <Link href={`/posts/${id}`} dangerouslySetInnerHTML={{ __html: title }}></Link>
+                            <Link href={`/posts/${id}`} className={utilStyles.noDecoration} dangerouslySetInnerHTML={{ __html: title }}></Link>
                             <br />
                             <small className={utilStyles.lightText}>
                                 <Date dateString={date} />
