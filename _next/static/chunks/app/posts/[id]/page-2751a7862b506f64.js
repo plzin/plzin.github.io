0cbaaf7fc65d98b42bc382160a4318af6514d066
plzin <plzin@users.noreply.github.com>
2025-06-17 18:17:53 +0000

    deploy: a080093c089261d5da731dffa05ee0a0c0066d64

---
+++ _next/static/chunks/app/posts/[id]/page-2751a7862b506f64.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[880],{2012:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,6800,23))},6800:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[385,874,441,684,358],()=>s(2012)),_N_E=e.O()}]);
